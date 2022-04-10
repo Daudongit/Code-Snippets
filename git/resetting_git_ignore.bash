6bbf238f2e5411f787cbf23d871d919b1ec43290
Daudongit <daudonmail@gmail.com>
2022-04-10 17:01:33 +0100

    Update resetting_git_ignore.bash

--- git/resetting_git_ignore.bash
+++ git/resetting_git_ignore.bash
@@ -8,3 +8,10 @@ git add .
 
 #commit the new change
 git commit -m ".gitignore is now working"
+
+# remove only one folder
+echo "node_modules" >> .gitignore
+
+git rm -r --cached node_modules
+
+git commit -am 'untracked node_modules'
